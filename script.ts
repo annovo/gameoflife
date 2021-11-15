dd461275128be1c242d67f6da39efa8c57332489
Anastasiia Novoselova <ananas.nasya@gmail.com>
2021-11-15 15:07:55 -0800

    add pattern

--- script.ts
+++ script.ts
@@ -1,5 +1,3 @@
-const container: HTMLElement = document.getElementById('container')
-
 class GameOfLife {
    color: string = '#3498db'
    cells: boolean[][]
@@ -21,33 +19,9 @@ class GameOfLife {
    }
 
    createField(): void {
-      document?.addEventListener('keydown', (e: KeyboardEvent) => {
-         if(e.defaultPrevented)
-            return
-         if(e.code === 'Enter') {
-            this.start = !this.start
-         } else if(e.code === 'Escape') 
-            this.reset()
-         
-         e.preventDefault
-      })
+      this.addEventListeners()
 
-      const startBtn: HTMLElement = document?.getElementById('start')
-      startBtn.addEventListener('click', (e) => {
-         if(e.defaultPrevented)
-            return
-         this.start = !this.start
-         startBtn.textContent = this.start ? 'Stop' : 'Start'
-         e.preventDefault()
-      })
-
-      document?.getElementById('reset').addEventListener('click', (e) => {
-         if(e.defaultPrevented)
-            return
-         this.reset()
-         e.preventDefault()
-      })
-
+      const container: HTMLElement = document.getElementById('container')
       for(let i = 0; i < this.m; i++) {
          for(let j = 0; j < this.n; j++) {
             const square: HTMLElement = document?.createElement('div')
@@ -80,7 +54,12 @@ class GameOfLife {
 
             container.appendChild(square)
          }       
-     }
+      }
+
+      let y = Math.floor(this.cells.length / 2)
+      let x = Math.floor(this.cells[0].length / 2)
+      this.initPattern(x, y)
+      this.initPattern(x - 3, y - 3)
    }
 
    setColor(element: HTMLElement): void {
@@ -165,9 +144,47 @@ class GameOfLife {
          }));
       }
    }
+
+   initPattern(x: number, y: number): void {
+      for(let i = y; i < y + 3; i++) {
+         for(let j = x; j < x + 3; j++) {
+            this.cells[i][j] = true;
+            this.setColor(document?.getElementById(`square-${i}-${j}`))
+         }
+      }
+   }
+
+   addEventListeners(): void {
+      document?.addEventListener('keydown', (e: KeyboardEvent) => {
+         if(e.defaultPrevented)
+            return
+         if(e.code === 'Enter') {
+            this.start = !this.start
+         } else if(e.code === 'Escape') 
+            this.reset()
+         
+         e.preventDefault
+      })
+
+      const startBtn: HTMLElement = document?.getElementById('start')
+      startBtn.addEventListener('click', (e) => {
+         if(e.defaultPrevented)
+            return
+         this.start = !this.start
+         startBtn.textContent = this.start ? 'Stop' : 'Start'
+         e.preventDefault()
+      })
+
+      document?.getElementById('reset').addEventListener('click', (e) => {
+         if(e.defaultPrevented)
+            return
+         this.reset()
+         e.preventDefault()
+      })
+   }
 }
 
-const game = new GameOfLife();
-game.createField();
-game.play.bind(game)();
+const game = new GameOfLife()
+game.createField()
+game.play.bind(game)()
 
